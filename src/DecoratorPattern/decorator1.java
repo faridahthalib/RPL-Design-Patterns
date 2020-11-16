344517a5862296f8d0d686f1aee5c0a0e773e610
Ananda <anandapitan@gmail.com>
2020-11-16 20:29:02 +0800

    Menambahkan packages decorator dan interface dari desain pattern decorator. Lalu menambahkan fungsi NilaiSumber di main class untuk menampilkan nilai dan sumber cerita

---
+++ src/DecoratorPattern/decorator1.java
@@ -0,0 +1,18 @@
+package DecoratorPattern;
+
+public interface decorator1 {
+    public String getCerita1();
+
+    
+    public String getCerita2();
+
+    
+    public String getCerita3();
+
+    
+    public String getCerita4();
+
+    
+    public String getCerita5();
+
+}
