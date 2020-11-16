5606caf31266d6b51436d45355f2aacf8a6add2e
Faridah <faridah.thalib@gmail.com>
2020-11-16 11:22:04 +0800

    Menuliskan code untuk get object dari class yang akan ditambahkan nantinya yaitu factory 1-5

--- src/designpatternsrpl/FactoryPattern.java
+++ src/designpatternsrpl/FactoryPattern.java
@@ -10,5 +10,27 @@ package designpatternsrpl;
  * @author Faridah
  */
 public class FactoryPattern {
-    
+    //menggunakan getCerita untuk get object dari cerita daerah
+    public factory getCerita (String ceritaDaerah){
+        if(ceritaDaerah == null){
+            return null;
+        }
+        if(ceritaDaerah.equalsIgnoreCase("CERITA1")){
+            return new factory1();
+        
+        } else if(ceritaDaerah.equalsIgnoreCase("CERITA2")){
+            return new factory2();
+            
+        } else if(ceritaDaerah.equalsIgnoreCase("CERITA3")){
+            return new factory3();
+            
+        } else if(ceritaDaerah.equalsIgnoreCase("CERITA4")){
+            return new factory4();
+            
+        } else if(ceritaDaerah.equalsIgnoreCase("CERITA5")){
+            return new factory5();
+        }
+        
+        return null;
+    }
 }
