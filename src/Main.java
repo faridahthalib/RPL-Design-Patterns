dce3153d8df340d6aa87a806c4e3d663e8df5a72
Faridah <faridah.thalib@gmail.com>
2020-11-16 18:45:30 +0800

    Membuat package baru untuk masing-masing pattern dan memindah class main, lalu menambahkan import agar class-class yang ada di package lain dapat diakses main class

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package designpatternsrpl;
 
+
+import FactoryPattern.FactoryPattern;
+import FactoryPattern.factory;
 import static java.lang.System.exit;
 import java.util.Scanner;
 
